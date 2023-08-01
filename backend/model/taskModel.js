164d11f5b3e119b2950d93f93728ff152396b3f2
MAITUMELO RADIPODI <mradipodi@gmail.com>
2023-08-01 19:36:00 +0200

    new commit

--- backend/model/taskModel.js
+++ backend/model/taskModel.js
@@ -1,22 +1,22 @@
-const mongoose  = require('mongoose')
+const mongoose = require('mongoose');
 
 const taskSchema = mongoose.Schema(
     {
         name: {
             type: String,
-            required: [true,"Please add a task"]
+            required: [true, 'Please add a task'],
         },
-        completed:{
-             type: Boolean,
+        completed: {
+            type: Boolean,
             required: true,
-            default:false
-        }
+            default: false,
+        },
     },
     {
-        timestamps:true
+        timestamps: true,
     }
-)
+);
 
-const Task = mongoose.model("Task",taskSchema)
+const Task = mongoose.model('Task', taskSchema);
 
-module.exports = Task
+module.exports = Task;
--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const dotenv = require('dotenv').config()
 const express = require('express')
-const connectDB  = require('./backend/config/connectDB')
+const connectDB = require('./backend/config/connectDB')
 const Task = require('./backend/model/taskModel')
-const app = express();
-
+const app = express()
 
 //Middleware
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 /* const logger = (req,res,next) => {
     console.log("middleware ran")
     console.log(req.method)
@@ -15,30 +14,26 @@ app.use(express.urlencoded({extended: false}))
 } */
 
 //Routes
-app.get("/",(req,res) => {
-    res.send("Home Page")
+app.get('/', (req, res) => {
+    res.send('Home Page')
 })
-
 
 //create a  task,post request
 
-app.post('/api/tasks',async (req,res) => {
-  /*   console.log(req.body)
+app.post('/api/tasks', async (req, res) => {
+    /*   console.log(req.body)
     res.send("Task Created") */
-    try{
-          const task = await Task.create(req.body)
-          res.status(200).json(task)
-
-    }catch(error){
-      res.status(500).json({msg: error.message})
-
+    try {
+        const task = await Task.create(req.body)
+        res.status(200).json(task)
+    } catch (error) {
+        res.status(500).json({ msg: error.message })
     }
-
 })
 
 connectDB()
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT,() => {
+app.listen(PORT, () => {
     console.log('server running on port 5000 ')
 })
